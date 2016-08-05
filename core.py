3bd6fe2755a03c8532451f0f9d05f3c7a48e5840
aleksey <aleksey@risk-ai.com>
2016-08-05 13:17:01 -0400

    core

--- core.py
+++ core.py
@@ -1,5 +1,14 @@
 import sys
+import os
+from datetime import datetime
 import logging
+
+
+# Get available disk space
+def free_space():
+    st = os.statvfs(".")
+    du = st.f_bavail * st.f_frsize
+    return du
 
 def capture_ex(fun):
     """
@@ -11,6 +20,30 @@ def capture_ex(fun):
         except Exception as ex:
             log.error("exception at {0} msg: {1}", fun, str(ex))
     return wrapper
+
+class Location:
+
+    def __init__(self):
+        self.lat = 0
+        self.lng = 0
+        self.alt = 0
+        self.time = datetime.now()
+
+    @capture_ex
+    def update(self,gpsline):
+
+        toks = gpsline.split(',')
+
+        if toks[6] != "0":
+            self.time = datetime.strptime(toks[1][:6],"%H%M%S")
+            lng_s = 1 if toks[3] == 'N' else -1
+            lat_s = 1 if toks[5] == 'E' else -1
+            self.lat = parse_degrees(toks[2]) * lng_s
+            self.lng = parse_degrees(toks[4]) * lat_s
+            self.alt =float(toks[9])
+
+    def __repr__(self):
+        return str.format("{2}:{0},{1}", self.lat, self.lng,self.time)
 
 def setup_logging(*,name,fileName, levelName=None):
 	root = logging.getLogger()
